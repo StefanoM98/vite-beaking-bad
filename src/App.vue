cf8645be39dce1ffdd23a332ece79103b6e88429
StefanoM98 <smartino030@gmail.com>
2023-04-17 14:07:31 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import { store } from "./store";
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+  },
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss";
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <h1>Yu-Gi-Oh API</h1>
+</template>
+
+<style scoped lang="scss">
+h1 {
+  font-weight: 900;
+}
+</style>
